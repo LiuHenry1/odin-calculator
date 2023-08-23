ab750e3829ac2da080e58d3a67b433a509f414d6
Henry Liu <hliu4176@gmail.com>
2023-08-23 17:23:32 -0500

    Implement logic to accumulate operand
    Implement function 'appendDigit' to build up operand; implement
    function 'setUpDigitEventListeners' to attach event listeners to the
    interface.

--- script.js
+++ script.js
@@ -14,3 +14,24 @@ function divide(a, b) {
     return a / b;
 }
 
+// TODO: write logic to accumulate operand when a number is pressed
+// 1) attach handlers
+function setUpDigitEventListeners() {
+    const digits = document.getElementsByClassName('digit');
+    Array.from(digits).forEach(digit => digit.addEventListener('click', () => {
+        appendDigit(digit.value);
+    }));
+}
+
+// 2) implement callback
+function appendDigit(digit) {
+    const accumulator = document.getElementById('accumulator');
+    
+    if (accumulator.textContent === '0') 
+        accumulator.textContent = digit;
+    else 
+        accumulator.textContent += digit;
+}
+
+setUpDigitEventListeners();
+
